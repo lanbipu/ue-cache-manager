aa71a74d770ac6db87c4d31e3f93336054f3347e
lanbipu1988 <lanbipu@gmail.com>
2026-05-01 22:01:52 +0800

    feat(rust): add CredentialRecord struct with insert/list/find/delete CRUD

--- src-tauri/src/data/mod.rs
+++ src-tauri/src/data/mod.rs
@@ -1,10 +1,12 @@
 pub mod connection;
+pub mod credentials;
 pub mod machine_gpus;
 pub mod machine_ue_installs;
 pub mod machines;
 pub mod schema;
 
 pub use connection::{open, open_in_memory, Db};
+pub use credentials::CredentialRecord;
 pub use machine_gpus::GpuInfo;
 pub use machine_ue_installs::UeInstall;
 pub use machines::Machine;
