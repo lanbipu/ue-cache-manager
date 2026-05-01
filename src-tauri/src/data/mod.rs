d333e13d378bbea7d632fb6198a377a4434afbb7
lanbipu1988 <lanbipu@gmail.com>
2026-05-01 21:59:18 +0800

    feat(rust): add GpuInfo struct with insert/list/replace_for_machine CRUD

--- src-tauri/src/data/mod.rs
+++ src-tauri/src/data/mod.rs
@@ -1,8 +1,10 @@
 pub mod connection;
+pub mod machine_gpus;
 pub mod machine_ue_installs;
 pub mod machines;
 pub mod schema;
 
 pub use connection::{open, open_in_memory, Db};
+pub use machine_gpus::GpuInfo;
 pub use machine_ue_installs::UeInstall;
 pub use machines::Machine;
