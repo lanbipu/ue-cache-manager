c21c9ec43c991e5fcd3e0a99de29047dddab1cc2
lanbipu1988 <lanbipu@gmail.com>
2026-05-05 13:53:51 +0800

    feat(data): scan_runs CRUD (insert/finish/find_by_id/list_recent)

--- src-tauri/src/data/mod.rs
+++ src-tauri/src/data/mod.rs
@@ -3,6 +3,7 @@ pub mod credentials;
 pub mod machine_gpus;
 pub mod machine_ue_installs;
 pub mod machines;
+pub mod scan_runs;
 pub mod schema;
 pub mod share_configs;
 
@@ -11,4 +12,5 @@ pub use credentials::{CredentialKind, CredentialRecord};
 pub use machine_gpus::{GpuInfo, GpuVendor};
 pub use machine_ue_installs::UeInstall;
 pub use machines::Machine;
+pub use scan_runs::ScanRun;
 pub use share_configs::{ShareConfig, ShareMode};
