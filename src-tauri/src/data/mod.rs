37a155e1446d1010fb3b00116843b21d1f3f056f
lanbipu1988 <lanbipu@gmail.com>
2026-05-05 13:57:15 +0800

    feat(data): ini_findings CRUD + severity counts

--- src-tauri/src/data/mod.rs
+++ src-tauri/src/data/mod.rs
@@ -1,5 +1,6 @@
 pub mod connection;
 pub mod credentials;
+pub mod ini_findings;
 pub mod machine_gpus;
 pub mod machine_ue_installs;
 pub mod machines;
@@ -9,6 +10,7 @@ pub mod share_configs;
 
 pub use connection::{open, open_in_memory, Db};
 pub use credentials::{CredentialKind, CredentialRecord};
+pub use ini_findings::{IniFinding, SeverityCounts};
 pub use machine_gpus::{GpuInfo, GpuVendor};
 pub use machine_ue_installs::UeInstall;
 pub use machines::Machine;
